50ddf33d15e560eb2cb621d67f809f92675e63fa
= <stephen.carlylesmith@googlemail.com>
2021-11-20 18:12:20 +0000

    Initial commit

---
+++ src/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const TARGET_FPS = 60
+const ACCELERATION = 80
+const MAX_SPEED = 64
+const FRICTION = 100
+const AIR_RESISTANCE = 1
+const GRAVITY = 4
+const JUMP_FORCE = 140
+
+var motion = Vector2.ZERO
+
+onready var sprite = $Sprite
+onready var animationPlayer = $AnimationPlayer
+
+func _physics_process(delta):
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if x_input != 0:
+		animationPlayer.play("Run")
+		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		sprite.flip_h = x_input < 0
+	else:
+		animationPlayer.play("Stand")
+	
+	motion.y += GRAVITY * delta * TARGET_FPS
+	
+	if is_on_floor():
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
+			
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
+	else:
+		animationPlayer.play("Jump")
+		
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE#/2
+		
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+	
+	motion = move_and_slide(motion, Vector2.UP)
