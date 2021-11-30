ed8d70076228dc51dd8f407ce7481dca9d399770
= <stephen.carlylesmith@googlemail.com>
2021-11-30 17:36:01 +0000

    Fireballs all same colour

--- src/AlternateColours.gd
+++ src/AlternateColours.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var interval: float
+export var flash : bool = false
 
 var time: float
 
@@ -11,8 +12,15 @@ var colors = [Color(1.0, 0.0, 0.0, 1.0),
 		  Color(1.0, 0.0, 1.0, 1.0),
 		  Color(0.0, 0.0, 1.0, 1.0)]
 		
-		
+func _ready():
+	self.get_parent().modulate = colors[Globals.rnd.randi_range(0, colors.size()-1)];
+	pass
+	
+	
 func _process(delta):
+	if flash == false:
+		return
+		
 	time += delta
 	if time > interval:
 		time = 0
