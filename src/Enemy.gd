e2fb5eb6d71db288ef9f71a0b30f0080fd16642f
= <stephen.carlylesmith@googlemail.com>
2021-12-02 20:26:55 +0000

    Added Fuzzies

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,35 +1,75 @@
 extends Node2D
 
 const SPEED = 90
+enum Type {Fireball, Fuzzy}
 
 onready var main = get_tree().get_root().get_node("World")
 
-var x_dir : int
+var type
+var x_dir : float
 var y_dir : float
 
 func _ready():
 	self.position.x = 512
-	self.position.y = Globals.rnd.randi_range(10, 300)
-	x_dir = 1
-	if Globals.rnd.randi_range(0, 1) == 1:
-		x_dir = -1;
-		$Rocket.flip_h = true
-		$Fireball.flip_h = true
-	y_dir = Globals.rnd.randf_range(-1, 1)
+	self.position.y = Globals.rnd.randi_range(10, 300)#
+
+	$Fireball.visible = false
+	$Fuzzy.visible = false
+		
+	var t = Globals.rnd.randi_range(0, 1)
+	if t == 0:
+		type = Type.Fireball
+		$Fireball.visible = true
+		x_dir = 1
+		if Globals.rnd.randi_range(0, 1) == 1:
+			x_dir = -1;
+			$Rocket.flip_h = true
+			$Fireball.flip_h = true
+		y_dir = Globals.rnd.randf_range(-.5, .5)
+	elif t == 1:
+		type = Type.Fuzzy
+		$Fuzzy.visible = true
+		change_fuzzy_dir()
 	pass
 
 
+func change_fuzzy_dir():
+	x_dir = 1
+	y_dir = 1
+	if Globals.rnd.randi_range(0, 1) == 1:
+		x_dir = -1;
+	if Globals.rnd.randi_range(0, 1) == 1:
+		y_dir = -1;
+	pass
+	
+	
 func _physics_process(delta):
 	self.position.x -= SPEED * delta * x_dir
-	self.position.y -= (SPEED/4) * delta * y_dir
+	self.position.y -= SPEED * delta * y_dir
+	
+	if position.y < 20:
+		self.position.y += 1
+		y_dir = y_dir * -1
+		pass
 	pass
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("players"):
 		body.die()
-
-	main.show_explosion(self)
-	main.enemy_destroyed()
-	self.queue_free()
+		main.show_explosion(self)
+		self.queue_free()
+		return
+	elif body.is_in_group("lasers"):
+		main.show_explosion(self)
+		self.queue_free()
+		return
+		
+	if type == Type.Fuzzy:
+		change_fuzzy_dir()
+	else:
+		main.show_explosion(self)
+		main.enemy_destroyed()
+		self.queue_free()
+		pass
 	pass
--- src/Globals.gd
+++ src/Globals.gd
@@ -1,14 +1,11 @@
 extends Node
 
 const VERSION = "1.0"
-const RELEASE_MODE = false
+const RELEASE_MODE = true
 
 const NO_ENEMIES = true and !RELEASE_MODE
-#const PLAYER_INVINCIBLE = false and !RELEASE_MODE
 
 enum ObjectType {Fuel, Ship1, Ship2, Ship3}
-
-#const START_LIVES = 9
 
 # Other settings
 var MUSIC_ON = true
